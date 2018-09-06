dac93c03ffaf27439af3b13bd38f85dadd22ede7
Nathan Wolf <nathan@elmakers.com>
2018-09-06 09:05:30 -0700

    Add Grabbable script, which is causing me some issues

---
+++ Assets/Scripts/Grabbable.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.XR.MagicLeap;
+
+[RequireComponent(typeof(HandTracking))]
+public class Grabbable : MonoBehaviour
+{
+	public float RequiredConfidence = 0.9f;
+	
+	private Rigidbody _body;
+	
+	// Use this for initialization
+	void Start () {
+		_body = GetComponent<Rigidbody>();
+		_body.useGravity = false;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		if (MLHands.Right.KeyPoseConfidence >= RequiredConfidence)
+		{
+			Debug.Log(MLHands.Right.KeyPose);	
+		} 
+		else if (MLHands.Left.KeyPoseConfidence >= RequiredConfidence)
+		{
+			Debug.Log(MLHands.Left.KeyPose);
+		}
+	}
+	
+	void OnEnable()
+	{
+		MLResult result = MLHands.Start();
+		if (!result.IsOk)
+		{
+			Debug.LogError("Error GesturesExample starting MLHands, disabling script.");
+			enabled = false;
+		}
+	}
+	
+	void OnDisable()
+	{
+		MLHands.Stop();
+	}
+}
