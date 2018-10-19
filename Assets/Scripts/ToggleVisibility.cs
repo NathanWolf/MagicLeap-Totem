5e9dd4120a038d2fe927558951c90616d720cc5c
Nathan Wolf <nathan@elmakers.com>
2018-10-19 12:32:25 -0700

    Add script to toggle the world mesh visibility on and off

---
+++ Assets/Scripts/ToggleVisibility.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.XR.MagicLeap;
+
+public class ToggleVisibility : MonoBehaviour {
+	private Renderer _renderer;
+	public float RequiredConfidence = 0.9f;
+
+	// Use this for initialization
+	void Start ()
+	{
+		_renderer = GetComponent<MeshRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MLHandKeyPose pose = MLHandKeyPose.NoHand;
+		if (MLHands.Right.KeyPoseConfidence >= RequiredConfidence && MLHands.Right.KeyPose != MLHandKeyPose.NoHand)
+		{
+			pose = MLHands.Right.KeyPose;
+		} 
+		else if (MLHands.Left.KeyPoseConfidence >= RequiredConfidence)
+		{
+			pose = MLHands.Left.KeyPose;
+		}
+
+		switch (pose)
+		{
+			case MLHandKeyPose.Fist:
+				_renderer.enabled = true;
+				break;
+			case MLHandKeyPose.Thumb:
+				_renderer.enabled = false;
+				break;
+		}
+	}
+}
--- README.md
+++ README.md
@@ -8,4 +8,6 @@ Use the "C" gesture to drop totem into the world, and watch him bounce around.
 
 He should detect obstacles and change direction to navigate around. This is all very work-in-progress.
 
-If he gets stuck or jumps out of the world, use the "Ok" gesture to grab him again.
+If he gets stuck or jumps out of the world, use the "Pinch" gesture to grab him again.
+
+You can turn off the visibility of the world mesh (after meshing looks ok) with the thumbs up gesture.
